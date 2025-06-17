0f8133a1e21357069eb64ff05b7fba74ab1f736b
Fabian Stäger <fabian.staeger@uzh.ch>
2025-06-17 15:31:01 +0200

    fix run_local.py

--- src/spritz/scripts/run_local.py
+++ src/spritz/scripts/run_local.py
@@ -8,34 +8,27 @@ def main():
     era = an_dict["year"]
     runner_default = f"{get_fw_path()}/src/spritz/runners/runner_default.py"
     runner = an_dict.get("runner", runner_default)
+    runner = os.path.split(runner)[-1]
 
     txt = f"""
     #!/bin/bash
 
-    ERA=RPLME_ERA
     job_id=$1
 
     cd condor/job_${{job_id}}
 
-    # configs/.../condor/job_0/tmp
-
     mkdir tmp
     cd tmp
     cp ../chunks_job.pkl .
-    cp ../../run.sh .
-    cp {runner} runner.py
-    cp RPLME_FW_PATH/data/${{ERA}}/cfg.json .
+    cp ../../../config.py .
+    cp ../../{runner} .
+    cp ../../cfg.json .
 
-    ./run.sh 2> err 1> out
+    time python {runner} .
     cp results.pkl ../chunks_job.pkl
-    mv err ../err.txt
-    mv out ../out.txt
-    echo "Run locally" >> ../err.txt
-    echo "Done ${{job_id}}"
+    echo "Done ${job_id}"
     """
     with open("run_local.sh", "w") as file:
-        file.write(
-            txt.replace("RPLME_FW_PATH", get_fw_path()).replace("RPLME_ERA", era)
-        )
+        file.write(txt)
     proc = subprocess.Popen("chmod +x run_local.sh", shell=True)
     proc.wait()
