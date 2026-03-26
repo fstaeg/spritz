a846078b239f71a337a0552de232483a08e43284
Fabian Stäger <fabian.staeger@uzh.ch>
2026-03-26 18:17:52 +0100

    Add PSWeight systematics

--- src/spritz/modules/theory_unc.py
+++ src/spritz/modules/theory_unc.py
@@ -1,28 +1,31 @@
+import awkward as ak
+
 def theory_unc(events, variations):
     # QCD Scale
-    nVariations = len(events.LHEScaleWeight[0])
-    for i, j in enumerate(
-        [
-            0,
-            1,
-            2,
-            3,
-            nVariations - 1,
-            nVariations - 2,
-            nVariations - 3,
-            nVariations - 4,
-        ]
-    ):
-        events[f"weight_QCDScale_{i}"] = events.weight * events.LHEScaleWeight[:, j]
-        variations.register_variation(
-            columns=["weight"], variation_name=f"QCDScale_{i}"
-        )
+    if 'LHEScaleWeight' in ak.fields(events):
+        nVariations = len(events.LHEScaleWeight[0])
+        for i in range(nVariations):
+            events[f"weight_QCDScale_{i}"] = events.weight * events.LHEScaleWeight[:, i]
+            variations.register_variation(
+                columns=["weight"], variation_name=f"QCDScale_{i}"
+            )
 
     # Pdf Weights
-    nVariations = len(events.LHEPdfWeight[0])
-    for i, j in enumerate(range(nVariations)):
-        events[f"weight_PDFWeight_{i}"] = events.weight * events.LHEPdfWeight[:, j]
-        variations.register_variation(
-            columns=["weight"], variation_name=f"PDFWeight_{i}"
-        )
+    if 'LHEPdfWeight' in ak.fields(events):
+        nVariations = len(events.LHEPdfWeight[0])
+        for i in range(nVariations):
+            events[f"weight_PDFWeight_{i}"] = events.weight * events.LHEPdfWeight[:, i]
+            variations.register_variation(
+                columns=["weight"], variation_name=f"PDFWeight_{i}"
+            )
+
+    # PS Weights
+    if 'PSWeight' in ak.fields(events) and len(events.PSWeight)>0:
+        if len(events.PSWeight[0])==4:
+            for i in range(4):
+                events[f"weight_PSWeight_{i}"] = events.weight * events.PSWeight[:, i]
+                variations.register_variation(
+                    columns=["weight"], variation_name=f"PSWeight_{i}"
+                )
+    
     return events, variations
